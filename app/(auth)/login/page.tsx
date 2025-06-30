a6dc443b6b08bd9630be375e3577f814195b1a6f
Jorge Matos <jisapjisap@gmail.com>
2025-06-30 20:26:44 +0200

    Add logo image, enhance AuthLayout with navigation link and terms agreement, and update LoginPage to include email input and continue button.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,6 +4,8 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { FaGithub } from "react-icons/fa";
 import React from 'react'
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 const LoginPage = () => {
   return (
@@ -17,14 +19,28 @@ const LoginPage = () => {
         </CardDescription>
       </CardHeader>
 
-      <CardContent>
-        <Button className='w-full' variant="outline">
+      <CardContent className='flex flex-col gap-4'>
+        <Button className='w-full' variant='outline'>
           <FaGithub className='size-4 mr-2'/>
           Sign in with Github
         </Button>
 
         <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
           <span className='relative z-10 bg-card px-2 text-muted-foreground'>Or continue with</span>
+        </div>
+
+        <div className='grid gap-3'>
+          <div className='grid gap-2'>
+            <Label htmlFor="email">Email</Label>
+            <Input 
+              type='email' 
+              placeholder='your@email.com' 
+            />
+          </div>
+
+          <Button>
+            Continue with Email
+          </Button>
         </div>
       </CardContent>
     </Card>
