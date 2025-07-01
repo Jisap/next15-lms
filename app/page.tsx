50e19428004123bee6d81fb5ab7a70859dd83063
Jorge Matos <jisapjisap@gmail.com>
2025-07-01 21:47:09 +0200

    Refactor Home component to support authentication; fetch user session and display login button or user name accordingly.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import { ThemeToggle } from "@/components/themeToggle";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
   return (
-    <div>
+    <div className="p-24">
       <h1 className="text-2xl">hello world</h1>
       <ThemeToggle />
+
+      {session ? (
+        <p>{session.user.name}</p>
+      ):(
+        <Button>
+          Login
+        </Button>
+      )}
     </div>
   );
 }
