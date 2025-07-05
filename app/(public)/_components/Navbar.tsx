9a4fe2135669c6501b5c32eb3efaf1e9d8075f01
Jorge Matos <jisapjisap@gmail.com>
2025-07-05 20:55:49 +0200

    Add user authentication features to Navbar: Integrate session management and user dropdown for logged-in users, providing login and signup options for unauthenticated users. Introduce UserDropdown component for user-specific actions.

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from "next/link"
 import Logo from "@/public/logo.png"
 import Image from "next/image"
 import { ThemeToggle } from "@/components/themeToggle"
+import { authClient } from "@/lib/auth-client"
+import { buttonVariants } from "@/components/ui/button"
+import { UserDropdown } from "./UserDropdown"
 
 const navigationItems =[
   {
@@ -19,6 +24,9 @@ const navigationItems =[
 ]
 
 export const Navbar = () => {
+
+  const { data: session, isPending } = authClient.useSession();
+
   return (
     <header className="sticky top-0 z-50 w-full border bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
       <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
@@ -41,8 +49,21 @@ export const Navbar = () => {
             ))}
           </div>
 
-          <div>
+          <div className="flex items-center space-x-4">
             <ThemeToggle />
+
+            {isPending ? null : session ? (
+              <UserDropdown />
+            ) : (
+              <>
+                <Link href="/login" className={buttonVariants({ variant: "secondary"})}>
+                  Login
+                </Link>
+                <Link href="/login" className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </>
+            )}
           </div>
         </nav>
 
