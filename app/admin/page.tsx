21644e072faac2c538abc24824546fe465f4d65f
Jorge Matos <jisapjisap@gmail.com>
2025-07-08 15:52:41 +0200

    Refactor NavUser component to utilize session data for dynamic user information display, enhancing personalization. Update AppSidebar to remove hardcoded user data and improve sidebar structure. Add spacing in admin page layout for better visual organization.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -21,6 +21,7 @@ export default function Page() {
       }
     >
       <AppSidebar variant="inset" />
+      
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -28,17 +28,14 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import { authClient } from "@/lib/auth-client"
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
-  const { isMobile } = useSidebar()
+export function NavUser() {
+  const { isMobile } = useSidebar();
+
+  const { data: session, isPending } = authClient.useSession();
+
+  if(isPending) return null;
 
   return (
     <SidebarMenu>
@@ -49,14 +46,22 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage 
+                  src={session?.user.image ?? `https://avatar.vercel.sh/rauchg/${session?.user.email}`}
+                  alt={session?.user.name} 
+                />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate font-medium">
+                  {session?.user.name && session?.user.name.length > 0
+                    ? session?.user.name
+                    : session?.user.email.split("@")[0].charAt(0).toUpperCase()
+                  }
+                </span>
                 <span className="text-muted-foreground truncate text-xs">
-                  {user.email}
+                  {session?.user.email}
                 </span>
               </div>
               <IconDotsVertical className="ml-auto size-4" />
@@ -71,13 +76,26 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarImage 
+                    src={session?.user.image ?? `https://avatar.vercel.sh/rauchg/${session?.user.email}`} 
+                    alt={session?.user.name} 
+                  />
+                  <AvatarFallback className="rounded-lg">
+                    {session?.user.name && session?.user.name.length > 0 
+                      ? session?.user.name.charAt(0).toUpperCase()
+                      : session?.user.email.charAt(0).toUpperCase()
+                    }
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">
+                    {session?.user.name && session?.user.name.length > 0 
+                      ? session?.user.name
+                      : session?.user.email.split("@")[0].charAt(0).toUpperCase()
+                    }
+                  </span>
                   <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
+                    {session?.user.email}
                   </span>
                 </div>
               </div>
