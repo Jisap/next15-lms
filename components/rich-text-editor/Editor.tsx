56baefbc90bc03f625c1cbcc532833e6df2c9230
Jorge Matos <jisapjisap@gmail.com>
2025-07-11 16:46:54 +0200

    Enhance rich text editor functionality: Integrate text alignment features using Tiptap's extension, update Menubar for alignment options, and add typography support in globals.css. Update package dependencies for improved text formatting capabilities.

--- components/rich-text-editor/Editor.tsx
+++ components/rich-text-editor/Editor.tsx
@@ -1,20 +1,31 @@
 "use client"
 
-import { useEditor } from "@tiptap/react";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Menubar } from "./Menubar";
+import TextAlign from "@tiptap/extension-text-align";
 
 export const RichTextEditor = () => {
 
   const editor = useEditor({
     extensions: [
-      StarterKit
-    ]
+      StarterKit,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+
+    editorProps: {
+      attributes: {
+        class: "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-xl dark:prose-invert !w-full !max-w-none"
+      }
+    }
   })
 
   return (
-    <div>
+    <div className="w-full border border-input rounded-lg overflow-hidden dark:bg-input/30">
       <Menubar editor={editor} />
+      <EditorContent editor={editor} />
     </div>
   )
 }
--- components/rich-text-editor/Menubar.tsx
+++ components/rich-text-editor/Menubar.tsx
@@ -7,8 +7,10 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { Toggle } from "@/components/ui/toggle"
-import { BoldIcon, Heading, Heading1Icon, Heading2Icon, Heading3Icon, Italic, ListIcon, ListOrderedIcon, Strikethrough } from "lucide-react";
+import { AlignCenterIcon, AlignLeftIcon, AlignRightIcon, BoldIcon, Heading, Heading1Icon, Heading2Icon, Heading3Icon, Italic, ListIcon, ListOrderedIcon, RedoIcon, Strikethrough, UndoIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { text } from "stream/consumers";
+import { Button } from "../ui/button";
 
 interface iAppProps {
   editor: Editor | null;
@@ -19,9 +21,9 @@ export const Menubar = ({ editor }: iAppProps) => {
   if(!editor) return null;
 
   return (
-    <div className="border border-input rounded-t-lg p-2 bg-card flex flex-wrap gap-1 items-center">
+    <div className="border border-input border-t-0 border-x-0 rounded-t-lg p-2 bg-card flex flex-wrap gap-1 items-center">
       <TooltipProvider>
-        <div>
+        <div className="flex flex-wrap gap-1 ">
           {/* Bold */}
           <Tooltip>
             <TooltipTrigger asChild>
@@ -177,6 +179,108 @@ export const Menubar = ({ editor }: iAppProps) => {
         </div>
 
         <div className="w-px h-6 bg-border mx-2"></div>
+
+        <div className="flex flex-wrap gap-1 ">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive({textAlign: "left"})}
+                onPressedChange={() => editor.chain().focus().setTextAlign("left").run()}
+                className={cn(
+                  editor.isActive({ textAlign: "left" }) && "bg-muted text-muted-foreground"
+                )}
+              >
+                <AlignLeftIcon />
+              </Toggle>
+            </TooltipTrigger>
+
+            <TooltipContent>
+              Align Left
+            </TooltipContent>
+          </Tooltip>
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive({ textAlign: "center" })}
+                onPressedChange={() => editor.chain().focus().setTextAlign("center").run()}
+                className={cn(
+                  editor.isActive({ textAlign: "center" }) && "bg-muted text-muted-foreground"
+                )}
+              >
+                <AlignCenterIcon />
+              </Toggle>
+            </TooltipTrigger>
+
+            <TooltipContent>
+              Align Center
+            </TooltipContent>
+          </Tooltip>
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Toggle
+                size="sm"
+                pressed={editor.isActive({ textAlign: "right" })}
+                onPressedChange={() => editor.chain().focus().setTextAlign("right").run()}
+                className={cn(
+                  editor.isActive({ textAlign: "right" }) && "bg-muted text-muted-foreground"
+                )}
+              >
+                <AlignRightIcon />
+              </Toggle>
+            </TooltipTrigger>
+
+            <TooltipContent>
+              Align Right
+            </TooltipContent>
+          </Tooltip>
+        </div>
+
+        <div className="w-px h-6 bg-border mx-2"></div>
+
+        <div className="flex flex-wrap gap-1 ">
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                size="sm"
+                variant="ghost"
+                type="button"
+                onClick={() => editor.chain().focus().undo().run()}
+                disabled={!editor.can().undo()}
+              >
+                <UndoIcon />
+              </Button>
+            </TooltipTrigger>
+
+            <TooltipContent>
+              Undo
+            </TooltipContent>
+          </Tooltip>
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                size="sm"
+                variant="ghost"
+                type="button"
+                onClick={() => editor.chain().focus().redo().run()}
+                disabled={!editor.can().redo()}
+              >
+                <RedoIcon />
+              </Button>
+            </TooltipTrigger>
+
+            <TooltipContent>
+              Redo
+            </TooltipContent>
+          </Tooltip>
+        
+
+        </div>
       </TooltipProvider>
     </div>
   )
