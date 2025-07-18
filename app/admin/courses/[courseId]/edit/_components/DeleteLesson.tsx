ef7d032c08284728bd49954d3f12df23effc6749
Jorge Matos <jisapjisap@gmail.com>
2025-07-18 17:24:07 +0200

    Enhance DeleteLesson component with confirmation dialog: Add AlertDialogContent to the DeleteLesson component, providing users with a clear confirmation prompt before deleting a lesson. This improves user experience by ensuring actions are intentional and irreversible.

--- app/admin/courses/[courseId]/edit/_components/DeleteLesson.tsx
+++ app/admin/courses/[courseId]/edit/_components/DeleteLesson.tsx
@@ -1,4 +1,5 @@
-import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog"
+import { Alert } from "@/components/ui/alert";
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button";
 import { Trash2 } from "lucide-react";
 import { useState } from "react";
@@ -17,6 +18,22 @@ export const DeleteLesson = ({ lessonId }: { lessonId: string }) => {
           <Trash2 className="size-4" />
         </Button>
       </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure ?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the lesson and all its data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button>
+            Delete
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
     </AlertDialog>
   )
 }
