61efdac67470615876e1a358ea9e08e4f3a32c7e
Jorge Matos <jisapjisap@gmail.com>
2025-07-20 11:36:54 +0200

    Add PublicCourses route component: Create a new PublicCourses route in page.tsx to display a header and introductory text for exploring available courses, enhancing the user interface for course discovery.

---
+++ app/(public)/courses/page.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+const PublicCoursesroute = () => {
+  return (
+    <div className="mt-5">
+      <div className="flex flex-col space-y-2 mb-10">
+        <h1 className="text-3xl md:text-4xl font-bold tracking-tighter">
+          Explore Courses
+        </h1>  
+        <p className="text-muted-foregroun">
+          Discover our wide range of courses designed to help you achieve your learnings goals.
+        </p>
+      </div>  
+    </div>
+  )
+}
+
+export default PublicCoursesroute
