b0fd287b9420ce0c115e859a6bd56b3b7ebbac36
Jorge Matos <jisapjisap@gmail.com>
2025-07-20 11:41:59 +0200

    Add getAllCourses function to fetch published courses: Implement a new function in get-all-courses.ts to retrieve a list of published courses from the database, including relevant details such as title, price, and duration, enhancing data accessibility for course-related features.

---
+++ app/data/course/get-all-courses.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/db";
+
+
+
+export const getAllCourses = async () => {
+
+  const data = await prisma.course.findMany({
+    where: {
+      status: "Published"
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    select: {
+      title: true,
+      price: true,
+      smallDescription: true,
+      slug: true,
+      filekey: true,
+      id: true,
+      level: true,
+      duration: true,
+      category: true,
+    }
+  });
+
+  return data;
+}
