777fd85141924113ccd8b7ad016836e990ba5cc4
Jorge Matos <jisapjisap@gmail.com>
2025-07-20 10:36:57 +0200

    Add EmptyState component and update CoursesPage for no course scenario: Introduce EmptyState component to display a message when no courses are found, enhancing user experience. Update CoursesPage to conditionally render the EmptyState or course cards based on the data availability.

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -2,6 +2,7 @@ import { adminGetCourses } from "@/app/data/admin/admin-get-courses"
 import { buttonVariants } from "@/components/ui/button"
 import Link from "next/link"
 import { AdminCourseCard } from "./_components/AdminCourseCard"
+import { EmptyState } from "@/components/general/EmptyState"
 
 
 const CoursesPage = async() => {
@@ -18,11 +19,19 @@ const CoursesPage = async() => {
         </Link>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
-        {data.map((course) => (
-          <AdminCourseCard key={course.id} data={course} />
-        ))}
-      </div>
+      {data.length === 0 ? (
+        <EmptyState 
+          title="No courses found"
+          description="Create a new course to get started"
+          buttonText="Create Course"
+        />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+          {data.map((course) => (
+            <AdminCourseCard key={course.id} data={course} />
+          ))}
+        </div>
+      )}
     </>
   )
 }
