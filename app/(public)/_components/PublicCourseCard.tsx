914e6f1856901ee0a70440b677a06b174b6ac18c
Jorge Matos <jisapjisap@gmail.com>
2025-07-21 14:33:00 +0200

    Enhance PublicCourseCard component: Add course title, description, duration, and category display with links for more information. Update layout to include bottom margin for improved spacing.

--- app/(public)/_components/PublicCourseCard.tsx
+++ app/(public)/_components/PublicCourseCard.tsx
@@ -1,8 +1,11 @@
 import { PublicCourseType } from "@/app/data/course/get-all-courses"
 import { Badge } from "@/components/ui/badge"
-import { Card } from "@/components/ui/card"
+import { buttonVariants } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
 import { useConstructUrl } from "@/hooks/use-construct-url"
+import { School, TimerIcon } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 
 interface iAppProps {
   data: PublicCourseType
@@ -22,6 +25,39 @@ export const PublicCourseCard = ({data}: iAppProps) => {
         height={ 400 }
         className="w-full rounded-t-xl aspect-video h-full object-cover"
       />
+
+      <CardContent className="p-4">
+        <Link 
+          href={`/courses/${data.slug}`} 
+          className="font-medium text-lg line-clamp-2 hover:underline group-hover:text-primary transition-colors"
+        >
+          {data.title}
+        </Link>
+
+        <p className="line-clamp-2 text-sm text-muted-foreground leading-tight mt-2">
+          {data.smallDescription}
+        </p>
+
+        <div className="mt-4 flex items-center gap-x-5">
+          <div className="flex items-center gap-x-2">
+            <TimerIcon className="size-6 p-1 rounded-md text-primary bg-primary/10 "/>
+            <p className="text-sm text-muted-foreground">{data.duration}h</p>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <School className="size-6 p-1 rounded-md text-primary bg-primary/10 "/>
+            <p className="text-sm text-muted-foreground">{data.category}h</p>
+          </div>
+        </div>
+
+        <Link 
+          href={`/courses/${data.slug}`} 
+          className={buttonVariants({
+            className: "mt-4 w-full",
+          })}
+        >
+          Learn more
+        </Link>
+      </CardContent>
     </Card>
   )
 }
