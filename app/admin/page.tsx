4556fb738df1fad63c2f7a03219d96f63e4bc3a5
Jorge Matos <jisapjisap@gmail.com>
2025-07-25 17:23:26 +0200

    Enhance AdminIndexPage with loading skeleton: Import AdminCourseCardSkeleton for a better user experience during data fetching. Update Suspense fallback to display a skeleton layout while recent courses are loading.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { adminGetRecentCourses } from "../data/admin/admin-get-recent-courses";
 import { EmptyState } from "@/components/general/EmptyState";
-import { AdminCourseCard } from "./courses/_components/AdminCourseCard";
+import { AdminCourseCard, AdminCourseCardSkeleton } from "./courses/_components/AdminCourseCard";
 import { Suspense } from "react";
 
 
@@ -34,7 +34,7 @@ export default async function AdminIndexPage() {
           </Link>
         </div>
 
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<RenderRecentCoursesSkeletonLayout />}>
           <RenderRecentCourses />
         </Suspense>
       </div>
@@ -68,3 +68,12 @@ export const RenderRecentCourses = async() => {
     </div>
   )
 }
+
+export const RenderRecentCoursesSkeletonLayout = async() => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      {Array.from({ length: 2 }).map((_, index) => (
+        <AdminCourseCardSkeleton key={index} />  
+      ))}
+    </div>
+)}
