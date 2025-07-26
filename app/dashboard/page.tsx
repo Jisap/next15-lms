2f61b3649d31e919f1b33464020901509df56a3f
Jorge Matos <jisapjisap@gmail.com>
2025-07-26 16:30:22 +0200

    Remove data.json and implement course enrollment functionality: Delete the data.json file and introduce the getEnrolledCourses function to fetch active course enrollments for users. Update the DashboardPage component to display enrolled and available courses, enhancing user experience with conditional rendering for course availability.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,46 @@
+import { EmptyState } from "@/components/general/EmptyState";
+import { getAllCourses } from "../data/course/get-all-courses";
+import { getEnrolledCourses } from "../data/user/get-enrolled-courses";
+import { title } from 'process';
+import { description } from '../../components/sidebar/chart-area-interactive';
 
 
 
 
 
-const DashboardPage = () => {
+const DashboardPage = async() => {
+
+  const [courses, enrolledCourses] = await Promise.all([
+    getAllCourses(),
+    getEnrolledCourses(),
+   
+  ]);
+
   return (
-    <div>Hello World</div>
+    <>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">Enrolled Courses</h1>
+        <p className="text-muted-foreground">Here you can see all the courses you have access to.</p>
+      </div>
+
+      {enrolledCourses.length === 0 ? (
+        <EmptyState 
+          title="No Courses purchased"
+          description="You have not purchased any courses yet. Purchase a course to access it here."
+          buttonText="Browse Courses"
+          href="/courses"
+        />
+      ):(
+        <p>The courses you are enrolled in</p>
+      )}
+
+      <section className="mt-10">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-bold">Available Courses</h1>
+          <p className="text-muted-foreground">Here you can see all the courses you can purchase.</p>
+        </div>
+      </section>
+    </>
   )
 }
 
