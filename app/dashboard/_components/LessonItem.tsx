b52d0f371c8db9d70fb43627baabe2e9e89240f0
Jorge Matos <jisapjisap@gmail.com>
2025-07-27 10:45:22 +0200

    Enhance LessonItem component: Update layout to include a play icon and improved styling for lesson display, enhancing user interaction and visual appeal.

--- app/dashboard/_components/LessonItem.tsx
+++ app/dashboard/_components/LessonItem.tsx
@@ -1,6 +1,8 @@
 import { buttonVariants } from "@/components/ui/button"
 import Link from "next/link"
 import { description } from '../../../components/sidebar/chart-area-interactive';
+import { cn } from "@/lib/utils";
+import { Play } from "lucide-react";
 
 interface iAppProps {
   lesson: {
@@ -18,10 +20,27 @@ export const LessonItem = ({ lesson, slug }: iAppProps) => {
     <Link
       className={buttonVariants({
         variant: "outline",
+        className: cn("w-full p-2.5 h-auto justify-start transition-all")
       })}
       href="/"
     >
-      {lesson.title}
+      <div className="flex items-center gap-2.5 w-full min-w-0">
+        <div className="shrink-0">
+          <div className={cn(
+            "size-5 rounded-full border-2 bg-background flex justify-center items-center"
+          )}>
+            <Play className={cn(
+              "size-2.5 fill-current"
+            )}/>
+          </div>
+        </div>
+
+        <div>
+          <p>
+            {lesson.position}. {lesson.title}
+          </p>
+        </div>
+      </div>
     </Link>
   )
 }
