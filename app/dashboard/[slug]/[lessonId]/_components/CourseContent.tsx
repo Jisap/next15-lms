cf93dcc4fa7a7e6bb6d5e2c145dbddaacbb63e23
Jorge Matos <jisapjisap@gmail.com>
2025-07-28 17:13:35 +0200

    Implement VideoPlayer component in CourseContent: Add VideoPlayer functionality to display lesson videos with fallback messaging for missing content, enhancing user experience and interactivity.

--- app/dashboard/[slug]/[lessonId]/_components/CourseContent.tsx
+++ app/dashboard/[slug]/[lessonId]/_components/CourseContent.tsx
@@ -1,7 +1,8 @@
 import { LessonContentType } from "@/app/data/course/get-lesson-content"
 import { RenderDescription } from "@/components/rich-text-editor/RenderDescription"
 import { Button } from "@/components/ui/button"
-import { CheckCircle } from "lucide-react"
+import { useConstructUrl } from "@/hooks/use-construct-url"
+import { BookIcon, CheckCircle } from "lucide-react"
 
 
 interface iAppProps {
@@ -9,9 +10,51 @@ interface iAppProps {
 }
 
 export const CourseContent = ({ data }: iAppProps) => {
+
+  const VideoPlayer = ({
+    thumbnailKey,
+    videoKey,  
+  }:{
+    thumbnailKey: string,
+    videoKey: string
+  }) => {
+    
+    const videoUrl = useConstructUrl( videoKey );
+    const thumbnailUrl = useConstructUrl( thumbnailKey );
+
+    if(!videoKey) {
+      return(
+        <div className="aspect-video bg-muted rounded-lg flex flex-col items-center justify-center">
+          <BookIcon className="size-16 text-primary mx-auto mb-4" />
+          <p className="text-muted-foreground">
+            This lesson does not have a video yet
+          </p>
+        </div>
+      )
+    }
+
+    return (
+      <div className="aspect-video bg-black rounded-lg relative overflow-hidden">
+        <video 
+          className="w-full h-full object-cover"
+          controls
+          poster={thumbnailUrl}
+        >
+          <source src={videoUrl} type="video/mp4" />
+          <source src={videoUrl} type="video/webm" />
+          <source src={videoUrl} type="video/ogg" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-col h-full bg-background pl-6">
-      <h1>Video player</h1>
+      <VideoPlayer 
+        thumbnailKey={data.thumbnailKey ?? ""}
+        videoKey={data.videoKey ?? ""}
+      />
 
       <div className="py-4 border-b">
         <Button variant="outline">
