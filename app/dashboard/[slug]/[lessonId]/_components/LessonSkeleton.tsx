6be5c0f886535b16a0fe1bbad4a08cc65eacab1f
Jorge Matos <jisapjisap@gmail.com>
2025-07-29 17:36:35 +0200

    Finish

--- app/dashboard/[slug]/[lessonId]/_components/LessonSkeleton.tsx
+++ app/dashboard/[slug]/[lessonId]/_components/LessonSkeleton.tsx
@@ -13,6 +13,17 @@ export const LessonSkeleton = () => {
           <Skeleton className="h-8 w-3/4" />
           <Skeleton className="h-4 w-1/2" />
         </div>
+
+        <div className="space-y-3">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-5/6" />
+          <Skeleton className="h-4 w-4/6" />
+        </div>
+
+        <div className="flex gap-3">
+          <Skeleton className="h-10 w-32" />
+          <Skeleton className="h-10 w-5/24" />
+        </div>
       </div>
     </div>
   )
