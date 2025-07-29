f30e9a7dfd0085b33cfd2a4b14dfb2edb5bb3fcd
Jorge Matos <jisapjisap@gmail.com>
2025-07-29 17:09:07 +0200

    Refactor CourseSlugRoute to fetch course data and redirect to first lesson: Update CourseSlugRoute component to retrieve course sidebar data based on the slug, and implement redirection to the first lesson if available. Enhance user feedback with a message for courses without lessons.

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,8 +1,30 @@
+import { getCourseSidebarData } from "@/app/data/course/get-course-sidebar-data";
+import { fi } from "date-fns/locale";
+import { redirect } from "next/navigation";
 
 
-const CourseSlugRoute = () => {
+interface iAppProps {
+  params: Promise<{ slug: string }>
+  children: React.ReactNode
+}
+
+const CourseSlugRoute = async({ params, children }: iAppProps) => {
+
+  const { slug } = await params;
+
+  const course = await getCourseSidebarData(slug); // Obtiene los datos del curso en base al slug de la url para un usuario suscrito  
+  const firstChapter = course.course.chapter[0];
+  const firstLesson = firstChapter.lessons[0];
+
+  if(firstLesson){
+    redirect(`/dashboard/${slug}/${firstLesson.id}`)
+  }
+
   return (
-    <div>CourseSlugRoute</div>
+    <div className="flex items-center justify-center h-full text-center">
+      <h2 className="text-2xl font-bold mb-2">No lesson available</h2>
+      <p className="text-muted-foreground">This course does not have any lessons yet.</p>
+    </div>
   )
 }
 
